5e742f6b235a62d23c9de044d9da348877ace205
ravield <ravield1192@gmail.com>
2019-07-07 19:58:52 -0400

    animando menus

---
+++ RoleTeamGame/Assets/Marcotesting/UImanagerscene2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UImanagerscene2 : MonoBehaviour
+{
+    public RectTransform bkgr,boton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bkgr.DOAnchorPosY(0,0.5f);
+        boton.DOAnchorPosY(0,0.5f);
+    }
+
+}
