c265a205435bc62e11871cd80ca854c4e477a84a
ravield <ravield1192@gmail.com>
2019-07-07 19:14:33 -0400

    animando botones

---
+++ RoleTeamGame/Assets/Marcotesting/UImanager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UImanager : MonoBehaviour
+{
+    public RectTransform boton1,boton2,boton3; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        boton1.DOAnchorPosX(0,0.5f);
+        boton2.DOAnchorPosX(0,0.5f);
+        boton3.DOAnchorPosX(0,0.5f);
+    }
+
+}
