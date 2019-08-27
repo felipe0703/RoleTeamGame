4cddb9ea1c35c08600a67bcbcc6b6deb3613b21b
felipe0703 <felipe.aguilera.g@gmail.com>
2019-08-27 12:44:26 -0400

    .

--- RoleTeamGame/Assets/Scripts/Edifice.cs
+++ RoleTeamGame/Assets/Scripts/Edifice.cs
@@ -10,6 +10,45 @@ public class Edifice : MonoBehaviour
 
     public GameObject level1;
 
+    //AUDIO
+    private AudioSource edificeAudio;
+    public AudioClip[] clips;
+
+    public void Start()
+    {
+        edificeAudio = gameObject.GetComponent<AudioSource>();
+
+    }
+
+    public void Update()
+    {
+        
+        if (level1.activeSelf)
+        {
+            edificeAudio.clip = clips[0];
+        }
+        /*  Como no hay más nivel de fuego que el 1 no me puedo referir a los demas
+
+        if (level2.activeSelf)
+        {
+            edificeAudio.clip = clips[1];
+        }
+        if (level3.activeSelf)
+        {
+            edificeAudio.clip = clips[2];
+        }
+        if (level4.activeSelf)
+        {
+            edificeAudio.clip = clips[2];
+
+        }
+        */
+
+        if (!edificeAudio.isPlaying)
+        {
+            edificeAudio.Play();
+        }
+    }
 
     public void IsSelected()
     {
@@ -21,4 +60,6 @@ public class Edifice : MonoBehaviour
     {
         level1.SetActive(true);
     }
+
 }
+ 
