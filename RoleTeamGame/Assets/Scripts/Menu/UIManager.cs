5695d18edc4c4d765e17f97c9bd2153e45d4ef7f
ravield <ravield1192@gmail.com>
2019-09-12 15:34:46 -0300

    animacion menu bienvenida

--- RoleTeamGame/Assets/Scripts/Menu/UIManager.cs
+++ RoleTeamGame/Assets/Scripts/Menu/UIManager.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
+//using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    public RectTransform botonStart;
-    public RectTransform title;
+    //public RectTransform botonStart;
+   // public RectTransform title;
+
+    public GUIAnimFREE botonStart;
+    public GUIAnimFREE title;
 
     // Start is called before the first frame update
     void Start()
     {
-        title.DOAnchorPosY(160f , 0.5f);
-        botonStart.DOAnchorPosX(0, 0.5f);
+        //title.DOAnchorPosY(160f , 0.5f);
+        //botonStart.DOAnchorPosX(0, 0.5f);
+        botonStart.PlayInAnims(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        title.PlayInAnims(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
     }
 
 }
