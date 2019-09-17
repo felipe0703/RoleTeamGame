9e19f9f6f113f2f1ee35c7aa1fed7f5c17517287
ravield <ravield1192@gmail.com>
2019-09-17 15:24:48 -0300

    submenu animado con guianimator

--- RoleTeamGame/Assets/Scripts/Menu/UIManagerMainMenu.cs
+++ RoleTeamGame/Assets/Scripts/Menu/UIManagerMainMenu.cs
@@ -2,21 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
+//using DG.Tweening;
 
 public class UIManagerMainMenu : MonoBehaviour
 {
-    public RectTransform bkgr, buttonMultiplayer, buttonTutorial, buttonInstruction, buttonCredit;
+    //public RectTransform bkgr, buttonMultiplayer, buttonTutorial, buttonInstruction, buttonCredit;
+    public GUIAnimFREE bkgr,buttonMultiplayer,buttonTutorial,buttonInstruction;
     // Start is called before the first frame update
 
     void Start()
     {
-        bkgr.DOAnchorPosY(0, 0.2f);
-        buttonMultiplayer.DOAnchorPosX(-150 , 0.8f);
-        buttonTutorial.DOAnchorPosX(150, 0.8f);
-        buttonInstruction.DOAnchorPosX(150, 0.8f);
-        buttonCredit.DOAnchorPosY(-290, 0.8f);
-
+        //bkgr.DOAnchorPosY(0, 0.2f);
+        //buttonMultiplayer.DOAnchorPosX(-150 , 0.8f);
+        //buttonTutorial.DOAnchorPosX(150, 0.8f);
+        //buttonInstruction.DOAnchorPosX(150, 0.8f);
+        //buttonCredit.DOAnchorPosY(-290, 0.8f);
+        bkgr.PlayInAnims(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        buttonMultiplayer.PlayInAnims(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        buttonTutorial.PlayInAnims(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        buttonInstruction.PlayInAnims(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
     }
 
 }
