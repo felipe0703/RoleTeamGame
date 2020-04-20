e74957f9a76bfc1bbb19e67e32f6631831449483
Ekaitz Polledo <ekaitz.polledo@opendeusto.es>
2020-04-20 18:45:14 +0200

    Herencia para traducciones

--- RoleTeamGame/Assets/Scripts/i18n/I18nManager.cs
+++ RoleTeamGame/Assets/Scripts/i18n/I18nManager.cs
@@ -3,73 +3,71 @@ using UnityEngine.UI;
 using TMPro;
 using Mgl;
 
-public class I18nManager : MonoBehaviour {
-
-    private I18n i18n = I18n.Instance;
-
-    [SerializeField] private TMP_Dropdown LangInputField;
-
-    [SerializeField] private TextMeshProUGUI pressStart;
-    [SerializeField] private TextMeshProUGUI idioma;
-
-    private void Start()
+namespace Com.BrumaGames.Llamaradas
+{
+    public abstract class I18nManager : MonoBehaviour
     {
-        //Messenger.AddListener<string>("Language:Change", SetLanguage); // event when langauge change
-        InitLanguage();
-    }
 
-    public void ChangeLanguage()
-    {
-        var locale = "";
-        if(LangInputField.value == 0)
+        protected I18n i18n = I18n.Instance;
+
+        [SerializeField] private TMP_Dropdown LangInputField;
+
+        private void Start()
         {
-            locale = "en-US";
-        } else if (LangInputField.value == 1)
-        {
-            locale = "es-ES";
+            //Messenger.AddListener<string>("Language:Change", SetLanguage); // event when langauge change
+            InitLanguage();
         }
-        PlayerPrefs.SetString("Language", locale);
-        I18n.SetLocale(locale);
-        DoTranslations();
-    }
 
-    private void SetLanguage(string locale)
-    {
-        PlayerPrefs.SetString("Language", locale);
-        I18n.SetLocale(locale);
-        DoTranslations();
-    }
-
-    private void DoTranslations()
-    {
-        if (pressStart != null) pressStart.text = i18n.__("pressStart");
-        if (idioma != null) idioma.text = i18n.__("idioma");
-    }
-
-    private void InitLanguage()
-    {
-
-        if (PlayerPrefs.HasKey("Language"))
+        public void ChangeLanguage()
         {
-            SetLanguage(PlayerPrefs.GetString("Language"));
-
-            if (PlayerPrefs.GetString("Language") == "en-US")
+            var locale = "";
+            if (LangInputField.value == 0)
             {
-                LangInputField.value = 0;
+                locale = "en-US";
             }
-            else if (PlayerPrefs.GetString("Language") == "es-ES")
+            else if (LangInputField.value == 1)
             {
-                LangInputField.value = 1;
+                locale = "es-ES";
+            }
+            PlayerPrefs.SetString("Language", locale);
+            I18n.SetLocale(locale);
+            DoTranslations();
+        }
+
+        protected void SetLanguage(string locale)
+        {
+            PlayerPrefs.SetString("Language", locale);
+            I18n.SetLocale(locale);
+            DoTranslations();
+        }
+
+        protected abstract void DoTranslations();
+
+        protected void InitLanguage()
+        {
+
+            if (PlayerPrefs.HasKey("Language"))
+            {
+                SetLanguage(PlayerPrefs.GetString("Language"));
+
+                if (PlayerPrefs.GetString("Language") == "en-US")
+                {
+                    LangInputField.value = 0;
+                }
+                else if (PlayerPrefs.GetString("Language") == "es-ES")
+                {
+                    LangInputField.value = 1;
+                }
+
+                return;
             }
 
-            return;
-        }
+            // Sanity check:
+            if (!PlayerPrefs.HasKey("Language"))
+            {
+                SetLanguage("en-US");
+            }
 
-        // Sanity check:
-        if (!PlayerPrefs.HasKey("Language"))
-        {
-            SetLanguage("en-US");
         }
-
     }
 }
---
+++ RoleTeamGame/Assets/Scripts/i18n/I18nManagerMainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Mgl;
+
+namespace Com.BrumaGames.Llamaradas
+{
+    public class I18nManagerMainMenu : I18nManager
+    {
+        [SerializeField] private TextMeshProUGUI pressStart;
+        [SerializeField] private TextMeshProUGUI idioma;
+
+        protected override void DoTranslations()
+        {
+            if (pressStart != null) pressStart.text = i18n.__("pressStart");
+            if (idioma != null) idioma.text = i18n.__("idioma");
+        }
+    }
+}
