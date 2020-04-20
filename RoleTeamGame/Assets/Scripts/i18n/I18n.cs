122523711af850badf80eb7a8b6a515028895491
Ekaitz Polledo <ekaitz.polledo@opendeusto.es>
2020-04-20 16:57:49 +0200

    Preparación Translator

---
+++ RoleTeamGame/Assets/Scripts/i18n/I18n.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Com.BrumaGames.Llamaradas
+{
+    public class I18n : Mgl.I18n
+    {
+        protected new static readonly I18n instance = new I18n();
+
+        // Customize your languages here
+        protected new static string[] locales = new string[] {
+          "en-US",
+          "es-ES",
+        };
+
+        public new static I18n Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
