ad1d75e0bfd07ada0ea55bff4ca0e7c773c1bfb3
Oihane Zarate <oihanezarate@deusto.es>
2020-05-14 16:46:21 +0200

    Transalate RoomList

---
+++ RoleTeamGame/Assets/Scripts/Translate/I18MngRoomListEntry.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Mgl;
+
+
+namespace Com.BrumaGames.Llamaradas
+{
+    public class I18MngRoomListEntry : I18nMng
+    {
+
+        [SerializeField] private TextMeshProUGUI joinRandomButton;
+
+        protected override void DoTranslations()
+        {
+            if (joinRandomButton != null) joinRandomButton.text = i18n.__("joinRandomButton");
+        }
+    }
+}
